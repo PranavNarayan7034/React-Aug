ada00279271deb0b89ed940eb433913fdb23254e
PranavNarayan7034 <pranav@expertzlab.com>
2025-10-24 11:10:29 +0530

    oct24

--- usememo-usecallback/src/Components/Products/Products.jsx
+++ usememo-usecallback/src/Components/Products/Products.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import products from '../../Data/products'
+import './Product.css'
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
+    return (
+        <div className='container'>
+            <h1>Products</h1>
+            <div className='products'>
+                {products.map( (item) => (
+                    <div key={item.id} className='item'>
+                        <img src={item.image} />
+                        <h4>{item.name}</h4>
+                        <p>₹ {item.price}</p>
+                        <div className='btn'>
+                            <button>BuyNow</button>
+                            <button>AddToCart</button>
+                        </div>
+                    </div>
+                ) )}
+            </div>
+        </div>
+    )
 }
 
 export default Products
--- usememo-usecallback/src/Data/products.js
+++ usememo-usecallback/src/Data/products.js
@@ -8,31 +8,31 @@ const products = [
     "id": 1,
     "name": "Wireless Headphones",
     "price": 1999,
-    "image": {Speaker}
+    "image": Speaker
   },
   {
     "id": 2,
     "name": "Smart Watch",
     "price": 3499,
-    "image": {Mouse}
+    "image": Mouse
   },
   {
     "id": 3,
     "name": "Bluetooth Speaker",
     "price": 2499,
-    "image": {Keyboard}
+    "image": Keyboard
   },
   {
     "id": 4,
     "name": "Gaming Mouse",
     "price": 1299,
-    "image": {Watch}
+    "image": Watch
   },
   {
     "id": 5,
     "name": "Mechanical Keyboard",
     "price": 2999,
-    "image": {Headphone}
+    "image": Headphone
   }
 ]
 export default products
