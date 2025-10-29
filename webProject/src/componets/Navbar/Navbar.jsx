3d26f3c33ed677b7b059307b6c331bf2bd9c612c
PranavNarayan7034 <pranav@expertzlab.com>
2025-10-29 11:20:31 +0530

    Updated navbar

--- webProject/src/componets/Navbar/Navbar.jsx
+++ webProject/src/componets/Navbar/Navbar.jsx
@@ -3,8 +3,11 @@ import { IoSearchOutline } from "react-icons/io5";
 import { FiUser } from "react-icons/fi";
 import { IoIosArrowDown } from "react-icons/io";
 import './Navbar.scss'
-
+import { motion, AnimatePresence } from 'framer-motion'
+import { useState } from "react";
 const Navbar = () => {
+    const [dropDown, setDropdown] = useState(false)
+    // console.log("dropdown",dropDown)
     return (
         <div className='Navbar'>
             <div className="logo">
@@ -13,7 +16,28 @@ const Navbar = () => {
             <div className="navlinks">
                 <span>Home</span>
                 <span>AboutUs</span>
-                <span>Products <IoIosArrowDown /> </span>
+                <div className="dropdown"
+                    onMouseEnter={() => setDropdown(true)}
+                    onMouseLeave={() => setDropdown(false)}>
+
+                    <span>Products <IoIosArrowDown /> </span>
+                    <AnimatePresence>
+                        {dropDown && (
+                            <motion.div className="dropdown-menu"
+                                initial={{opacity:0,y:-100}}
+                                animate={{opacity:1,y:0}}
+                                exit={{opacity:0,y:-100}}
+                                transition={{duration:0.5}}
+                            >
+                                <span>Electronics</span>
+                                <span>Clothing</span>
+                                <span>Accessories</span>
+                                <span>Groceries</span>
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
+                </div>
+
                 <span>ContactUs</span>
             </div>
             <div className="auth">
